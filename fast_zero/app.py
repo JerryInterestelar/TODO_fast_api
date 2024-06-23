74afda9e7b7f0ed4bbc081246e30e769fc80f03b
JerryInterestellar <jerrydesousa17@gmail.com>
2024-06-23 16:27:03 -0300

    ADD - Rota DELETE delete_user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -51,3 +51,18 @@ def update_user(user_id: int, user: UserSchema):
     database[user_id - 1] = user_with_id
 
     return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    response_model=UserSchemaPublic,
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User Not Found',
+        )
+    user_with_id = database[user_id - 1]
+    del database[user_id - 1]
+    return user_with_id
