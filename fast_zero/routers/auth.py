53fb3be958e57c974f11fc9aff7c498e169d5c98
JerryInterestellar <jerrydesousa17@gmail.com>
2024-07-11 15:46:04 -0300

    ADD - Refresh de token

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -13,6 +13,7 @@ from fast_zero.schemas import (
 )
 from fast_zero.security import (
     create_access_token,
+    get_current_user,
     verify_password,
 )
 
@@ -44,5 +45,18 @@ def login_for_access_token(
 
     return {
         'access_token': access_token,
+        'token_type': 'bearer',
+    }
+
+
+@router.post(
+    '/refresh_token',
+    response_model=Token,
+)
+def refresh_access_token(user: User = Depends(get_current_user)):
+    new_access_token = create_access_token(data={'sub': user.username})
+
+    return {
+        'access_token': new_access_token,
         'token_type': 'bearer',
     }
